d1b5556ed04b20fa0e7a6851a2e6399485556729
cynamon jules <julescweb@mail.com>
2022-11-04 11:24:47 +0100

    animation du menu en js

--- assets/app.ts
+++ assets/app.ts
@@ -8,3 +8,6 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
+// script js
+import './scripts/header.ts';
+
---
+++ assets/scripts/header.ts
@@ -0,0 +1,14 @@
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.menu-xs');
+const content = document.querySelector('.app-content');
+
+burger.addEventListener('click', (event: MouseEvent) => {
+    menu.classList.toggle('hidden');
+});
+
+content.addEventListener('click', (event: MouseEvent) => {
+    if(!menu.classList.contains('hidden')) {
+        menu.classList.add('hidden');
+    }
+});
+
